f1f8bab6ac80614ae0e55dd1d4d679c90df98d22
jinhyukbae <qowlsgur133@gmail.com>
2023-03-13 15:26:18 +0900

    post 만듬.

---
+++ mysql_settings.py
@@ -0,0 +1,13 @@
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydb',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
+
+SECRET_KEY = 'django-insecure-w-#h96a*clf727+0+boogh!u4bhi@7aioga5w(9+nvb(+f%$(x'
