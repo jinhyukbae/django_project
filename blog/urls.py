e1d016a9bf2d16e41d666e356dbd4899ab9d9a69
jinhyukbae <qowlsgur133@gmail.com>
2023-03-14 11:46:20 +0900

    views function 방식인 것을 class 방식으로 변환.

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('<int:pk>/', views.single_post_page), # int 타입의 pk데이터(id값) 127.0.0.1/8000/1, 2, 3 ...
-    path('', views.index),
+    # path('<int:pk>/', views.single_post_page), # int 타입의 pk데이터(id값) 127.0.0.1/8000/1, 2, 3 ...
+    # path('', views.index),
+    path('', views.PostList.as_view()),
+    path('<int:pk>/', views.PostDetail.as_view()),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,35 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView, DetailView
+
+class PostList(ListView): #LISTVIEW 블로그 글 리스트 보기 장고 내장 함수
+    model = Post
+    ordering = '-pk'
+
+    # template_name = 'blog/index.html'
 
 # Create your views here.
-def index(request):
-    posts = Post.objects.all().order_by('-pk') # Post.objects.all() post안에 모든 (객체)내용을 가져옴
-    # Post.objects.all().order_by('-pk') -pk 프라이머리 키 (id)의 역순으로 정렬
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts': posts,
-        }
-    )
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk') # Post.objects.all() post안에 모든 (객체)내용을 가져옴
+#     # Post.objects.all().order_by('-pk') -pk 프라이머리 키 (id)의 역순으로 정렬
+#     return render(
+#         request,
+#         'blog/index.html',
+#         {
+#             'posts': posts,
+#         }
+#     )
 
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)
+class PostDetail(DetailView): # DetailView 상세보기 장고 내장 함수
+    model = Post
 
-    return render(
-        request,
-        'blog/single_post_page.html',
-        {
-            'post': post,
-        }
-    )
+# def single_post_page(request, pk):
+#     post = Post.objects.get(pk=pk)
+#
+#     return render(
+#         request,
+#         'blog/single_post_page.html',
+#         {
+#             'post': post,
+#         }
+#     )
