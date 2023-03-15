2637aa88bab0b81af2f6616bf26a0dc3fdc247fe
jinhyukbae <qowlsgur133@gmail.com>
2023-03-15 16:45:57 +0900

    대표이미지 파일 일반 파일 업로드 기능 추가.

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 
 import mysql_settings
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -123,6 +125,9 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
