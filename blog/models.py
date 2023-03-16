dc70e2348bdc1e1f4e87e9c8eafd3c20e5049c1c
jinhyukbae <qowlsgur133@gmail.com>
2023-03-16 17:51:31 +0900

    사용자 추가

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 import os
+
 
 # Create your models here.
 class Post(models.Model):
@@ -12,9 +14,10 @@ class Post(models.Model):
     head_image = models.ImageField(upload_to="blog/images/%Y/%m/%d", blank=True)
     file_upload = models.FileField(upload_to="blog/files/%Y/%m/%d", blank=True)
 
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'[{self.pk}]{self.title}'
+        return f'[{self.pk}]{self.title}::{self.author}' # 글제목(id=primarykey) 글번호::작성자
 
 
     def get_absolute_url(self):
